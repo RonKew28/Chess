f3ad114e782fce99131423a9294e0daed515cb22
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-05-02 15:07:50 -0400

    Create stepable module and create Knight class that implements stepable

--- pieces/knight.rb
+++ pieces/knight.rb
@@ -1,7 +1,22 @@
 require_relative 'piece'
+require_relative 'stepable'
 
 class Knight < Piece
+  include Stepable
+
+  KNIGHT_MOVE_DIRS = [[-2, -1],
+                  [-1, -2],
+                  [-2, 1],
+                  [-1, 2],
+                  [1, -2],
+                  [2, -1],
+                  [1, 2],
+                  [2, 1]]
   def symbol
     '♞'.colorize(color)
+  end
+
+  def move_diffs
+    KNIGHT_MOVE_DIRS
   end
 end
