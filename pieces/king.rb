b17340fe22f7e5074c836b89fa8ab6a008feddcc
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-05-02 15:12:07 -0400

    Create King class that implements Stepable module

--- pieces/king.rb
+++ pieces/king.rb
@@ -1,7 +1,27 @@
 require_relative 'piece'
-require 'colorize'
+require 'stepable'
+
 class King < Piece
+  include Stepable
+
+  KING_MOVE_DIRS = [
+                      [-1, -1],
+                      [-1, 0],
+                      [-1, 1],
+                      [0, -1],
+                      [0, 1],
+                      [1, -1],
+                      [1, 0],
+                      [1, 1]
+                    ]
   def symbol
      '♚'.colorize(color)
   end
+
+  protected
+
+  def move_diffs
+    KING_MOVE_DIRS
+  end
+  
 end
--- pieces/knight.rb
+++ pieces/knight.rb
@@ -4,14 +4,16 @@ require_relative 'stepable'
 class Knight < Piece
   include Stepable
 
-  KNIGHT_MOVE_DIRS = [[-2, -1],
-                  [-1, -2],
-                  [-2, 1],
-                  [-1, 2],
-                  [1, -2],
-                  [2, -1],
-                  [1, 2],
-                  [2, 1]]
+  KNIGHT_MOVE_DIRS = [
+                      [-2, -1],
+                      [-1, -2],
+                      [-2, 1],
+                      [-1, 2],
+                      [1, -2],
+                      [2, -1],
+                      [1, 2],
+                      [2, 1]
+                    ]
   def symbol
     '♞'.colorize(color)
   end
